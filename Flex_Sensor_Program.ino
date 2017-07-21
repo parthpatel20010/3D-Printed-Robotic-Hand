45fb634da099455c1f1dfe86e104ff63a939667f
parthpatel20010 <30298316+parthpatel20010@users.noreply.github.com>
2017-07-21 11:50:18 -0700

    Add files via upload

--- Flex_Sensor_Program.ino
+++ Flex_Sensor_Program.ino
@@ -4,9 +4,9 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
-int servoPin1 = 9;
-int servoPin2 = 5;
-int servoPin3 = 6; 
+int servoPin1 = 5;
+int servoPin2 = 6;
+int servoPin3 = 9; 
 int servoPin4 = 3; 
 int flexPin1 = 5; 
 int flexPin2 = 4; 
@@ -17,24 +17,24 @@ void setup() {
   pinMode(4, INPUT);
   pinMode(3, INPUT);
   pinMode(2, INPUT);
-  servo1.attach(flexPin1);
-  servo2.attach(flexPin2);
-  servo3.attach(flexPin3);
-  servo4.attach(flexPin4);
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
+  servo4.attach(servoPin4);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  servo1.write(map(analogRead(flexPin1), 630, 320, 170, 0));
+  servo1.write(map(analogRead(flexPin1), 380, 170, 140, 0));
   delay(50);
-  servo2.write(map(analogRead(flexPin2), 630, 320, 170, 0));
+  servo2.write(map(analogRead(flexPin2), 500, 300, 170, 0));
   delay(50);
-  servo3.write(map(analogRead(flexPin3), 630, 320, 170, 0));
+  servo3.write(map(analogRead(flexPin3), 430, 220, 170, 0));
   delay(50);
-  servo4.write(map(analogRead(flexPin4), 145, 80, 145, 5));
+  servo4.write(map(analogRead(flexPin4), 120, 80, 145, 5));
   delay(50);
-  Serial.println(analogRead(flexPin3));
+  Serial.println(analogRead(flexPin4));
 }
 
 
